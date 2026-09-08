e0426601b166f86ac7e3902df2ac230751b67e70
Fernando <ferkev230@gmail.com>
2026-09-08 13:26:46 -0600

    Prueba de commits

--- frontend/src/components/AddTrivia.tsx
+++ frontend/src/components/AddTrivia.tsx
@@ -8,7 +8,7 @@ export default function AddTrivia() {
     <div className='fixed cursor-pointer w-16 h-16 bg-purple-500 rounded-full shadow-md flex items-center justify-center text-white font-bold text-3xl right-5 bottom-20 hover:bg-purple-400 transition duration-300 ease-in-out hover:scale-105 hover:shadow-lg'
     onClick={() => navigate(`/add-trivia`)}
     >
-      +
+      + asd
     </div>
   )
 }
